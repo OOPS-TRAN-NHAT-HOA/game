491aab823ae83abda52c5ee16178896766dddf4d
peapea <daudaucsffdd@gmail.com>
2024-04-25 20:30:44 +0700

    modify and merge from helloimgnud

--- src/App.java
+++ src/App.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -75,6 +76,10 @@ public class App extends Application {
         startButton.setOnAction(e-> {gameloop.start();stage.setScene(gameScene);} );
 
         menuPane.getChildren().addAll(bg,title,exitButton,startButton);
+
+        // icon
+        Image icon = new Image("file:images\\chicken.png");
+        stage.getIcons().add(icon);
 
         //stage
         stage.setScene(menuScene);
